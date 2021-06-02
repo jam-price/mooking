5a8ff22b4f519ed75d58815db2cf8e0967aa6d30
emily doswell <emilydoswell@yahoo.co.uk>
2021-06-02 10:02:23 +0200

    Added booking#create action in bookings controller. Removed destroy and index actions from booking controller and from the bookings resources in routes.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,24 @@
-class BookingsController < ApplicationController
+class BookingsController < ApplicationController  
+  def new
+    @cow = Cow.find(params[:cow_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @cow = Cow.find(params[:cow_id])
+
+    @booking = Booking.new(booking_params)
+    @booking.cow = @cow
+    if @booking.save
+      redirect_to cow_path(@cow)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:cow).permit( :start_date, :end_date )
+  end
 end
