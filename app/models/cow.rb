ca5bebcb157e3d314c28b96213402889c19802b6
emily doswell <emilydoswell@yahoo.co.uk>
2021-06-03 12:14:16 +0200

    Added presence true validations to name, description, location and price of cow on Model

--- app/models/cow.rb
+++ app/models/cow.rb
@@ -2,4 +2,6 @@ class Cow < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
+
+  validates :name, :description, :location, :price, presence: true
 end
