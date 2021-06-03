de6d9859cd7e2200b46942e54a2526dffd3b2558
Chloe Avenas <avenaschloe@gmail.com>
2021-06-03 17:30:27 +0100

    fixed sound

--- app/javascript/packs/components/sound.js
+++ app/javascript/packs/components/sound.js
@@ -1,8 +1,10 @@
 const playSound = () => {
   const button = document.querySelector(".btn.btn-flat.moo");
+  console.log(button);
   if (button) {
     const audio = new Audio(button.dataset.audio);
     button.addEventListener("click", (event) => {
+      console.log(event);
       event.preventDefault();
       audio.play();
     });
