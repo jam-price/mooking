fa33188375a69234d0853aa77639bd750f874c72
James Price <jamesmpdev@gmail.com>
2021-06-03 11:21:12 +0100

    adding cows and their lat/long markers to homepage (in pages_controller)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
   def home
+    @cows = Cow.all
+
+    @markers = @cows.geocoded.map do |cow|
+      {
+        lat: cow.latitude,
+        lng: cow.longitude
+      }
   end
 end
