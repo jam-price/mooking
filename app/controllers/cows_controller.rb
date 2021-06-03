2d883e00a670d62784098dd5f7ae0072ce3657fb
emily doswell <emilydoswell@yahoo.co.uk>
2021-06-03 16:26:58 +0200

    delete button

--- app/controllers/cows_controller.rb
+++ app/controllers/cows_controller.rb
@@ -2,15 +2,12 @@ class CowsController < ApplicationController
   def index
     @cows = Cow.all
   end
-
   def show
     @cow = Cow.find(params[:id])
   end
-
   def new
     @cow = Cow.new
   end
-
   def create
     @cow = Cow.new(cow_params)
     @cow.user = current_user
@@ -20,9 +17,12 @@ class CowsController < ApplicationController
       render :new
     end
   end
-
+  def destroy 
+    @cow = Cow.find(params[:id])
+    @cow.destroy
+    redirect_to cows_path
+  end
   private
-
   def cow_params
     params.require(:cow).permit(
       :name, :description,
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :cows, only: [:index, :new, :create, :show] do
+  resources :cows, only: [:index, :new, :create, :show, :destroy] do
     resources :bookings, only: [ :new, :create ]
   end
   resources :bookings, only: [ :show, :index ]
